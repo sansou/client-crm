fbd12895f88a6b37dba10835e605b556af5ddd2d
Thales Souza <santos.thales89@gmail.com>
2024-12-12 09:40:46 -0300

    FEAT - list project and leads

---
+++ app/[id]/page.tsx
@@ -0,0 +1,33 @@
+export default async function Page({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const response = await fetch('http://localhost:3000/leads/project/a-qQzmV3')
+  const data = await response.json()
+  return (
+    <div className="overflow-x-auto">
+      <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+        <thead className="ltr:text-left rtl:text-right">
+          <tr>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Name</th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Created At</th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Email</th>
+            <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Phone</th>
+          </tr>
+        </thead>
+
+        <tbody className="divide-y divide-x divide-gray-200">
+          {data.map((l: any) => (
+            <tr>
+              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{l.name}</td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">{l.createdAt}</td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">{l.email}</td>
+              <td className="whitespace-nowrap px-4 py-2 text-gray-700">{l.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,86 +1,72 @@
-'use client'
-import ProjectForm from '@/componentes/projectForm';
-import { useState, useEffect } from 'react';
+import Link from "next/link"
 
-interface Project {
-  pk: string,
-  sk: string,
-  name: string,
-  status: string,
-  entityType: string;
-  createdAt?: Date,
-  updatadAt?: Date,
-}
+export default async function Page() {
+  let projects = [{
+    name: '1111',
+    pk: '11111'
+  }]
+  try {
+    const response = await fetch('http://localhost:3000/projects')
+    // let data: any = []
+    const data = await response.json()
+    console.log(data)
+    projects = data
+  } catch (error) {
+    console.error(error);
 
-interface Lead {
-  id: string;
-  name: string;
-}
-
-export default function Home() {
-  const [projects, setProjects] = useState<Project[]>([]);
-  const [error, setError] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
-  const [response, setResponse] = useState<boolean>(false);
-
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/projects');  // Substitua pela URL da sua API        
-        if (!response.ok) {
-          throw new Error('Falha na requisição');
-        }
-        const result = await response.json();
-        console.log('result', result);
-        
-        setProjects(result);
-      } catch (err) {
-        setError('Erro ao buscar dados');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  // const handleSubmit = async (e: React.FormEvent) => {
-  //   e.preventDefault();
-    
-  //   try {
-  //     const res = await fetch('http://localhost:3000/projects', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({ data: formData }),
-  //     });
-
-  //     if (!res.ok) {
-  //       throw new Error('Falha ao enviar dados');
-  //     }
-
-  //     const result = await res.json();
-  //     setResponse(result);
-  //   } catch (err) {
-  //     setError('Erro ao enviar dados');
-  //   }
-  // };
-  
+  }
   return (
-    <div className='container mx-auto px-4 py-16 justify-around'>
-    <div>
-      <ProjectForm></ProjectForm>
-    </div>
-      <ul>
-        {projects.map((project) => (
-          <li key={project.pk}>
-            {project.pk}{' '}
-            {/* <button onClick={() => goToLeads(project.pk)}>Gerenciar Leads</button> */}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    <>
+      <div>
+        {projects.map((p: any) =>
+          <Link
+            href={p.pk}
+            className="relative block overflow-hidden rounded-lg border border-gray-100 p-4 sm:p-6 lg:p-8"
+          >
+            <span
+              className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+            ></span>
+
+            <div className="sm:flex sm:justify-between sm:gap-4">
+              <div>
+                <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
+                  {p.name}
+                </h3>
+
+                <p className="mt-1 text-xs font-medium text-gray-600">By John Doe</p>
+              </div>
+
+              <div className="hidden sm:block sm:shrink-0">
+                <img
+                  alt=""
+                  src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+                  className="size-16 rounded-lg object-cover shadow-sm"
+                />
+              </div>
+            </div>
+
+            <div className="mt-4">
+              <p className="text-pretty text-sm text-gray-500">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit illum provident a, ipsa
+                maiores deleniti consectetur nobis et eaque.
+              </p>
+            </div>
+
+            <dl className="mt-6 flex gap-4 sm:gap-6">
+              <div className="flex flex-col-reverse">
+                <dt className="text-sm font-medium text-gray-600">Published</dt>
+                <dd className="text-xs text-gray-500">31st June, 2021</dd>
+              </div>
+
+              <div className="flex flex-col-reverse">
+                <dt className="text-sm font-medium text-gray-600">Reading time</dt>
+                <dd className="text-xs text-gray-500">3 minute</dd>
+              </div>
+            </dl>
+          </Link>
+
+        )}
+      </div>
+    </>
+  )
 }
